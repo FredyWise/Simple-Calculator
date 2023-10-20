4dc1dd640a1277aadc156f3ac59a33db81812ce4
FredyWise <fredywise333@gmail.com>
2023-10-20 20:55:53 +0800

    Update MainActivity.kt

--- app/src/main/java/com/fredy/simple_calculator/MainActivity.kt
+++ app/src/main/java/com/fredy/simple_calculator/MainActivity.kt
@@ -3,13 +3,17 @@ package com.fredy.simple_calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.fredy.simple_calculator.ui.theme.SimpleCalculatorTheme
 
@@ -28,8 +32,9 @@ class MainActivity: ComponentActivity() {
 @Composable
 fun Calculator_Screen(calculatorViewModel: calculatorViewModel = viewModel()) {
     Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.onBackground
+        color = Color.Black,
+        modifier = Modifier
+            .fillMaxSize()
     ) {
         Calculator(
             state = calculatorViewModel.state,
